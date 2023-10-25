beaac6835d599b21a20b51186728d0b04a5553ba
Ludmilla-H <ludmillaharnais31@gmail.com>
2023-10-25 14:37:55 -0400

    installation redux

--- App.jsx
+++ App.jsx
@@ -1,11 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import RoutesPetmatch from './src/Routes/RoutesPetmatch'
+import { Provider } from 'react-native-paper'
+import { store } from './src/Redux/store'
 
 const App = () => {
   return (
-    
-    <RoutesPetmatch/>    
+
+    <Provider store = {store}>
+    <RoutesPetmatch/>  
+    </Provider>  
   )
 }
 
